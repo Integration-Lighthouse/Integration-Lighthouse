62742e44a7bc4734141cb2337403232fbf4eb54c
Mojje R <mojtaba.rezaei@contica.se>
2024-11-02 22:05:44 +0100

    Feat: Add styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import logo from "./logo.jpg";
 import MarkDown from "markdown-to-jsx";
 
 function App() {
@@ -50,40 +51,74 @@ function App() {
   );
 
   return (
-    <div className="app">
-      <nav className="nav-list">
-        <h4>Integration Lighthouse</h4>
-        <input
-          type="text"
-          placeholder="Search titles..."
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className="search-input"
-        />
-        <ul>
-          {filteredArticles.map((firstLine, index) => (
-            <li key={index}>
-              <button
-                onClick={() =>
-                  handleArticleClick(firstLines.indexOf(firstLine))
-                }
-              >
-                {firstLine || "Loading..."}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="content-area">
-        {content ? (
-          <MarkDown>{content}</MarkDown>
-        ) : (
-          <div className="welcome-message">
-            <p>
-              Welcome to Integration Lighthouse!
-            </p>
+    <div class="app">
+      <header class="app-header">
+        <div class="app-header-logo">
+          <div class="logo">
+            <span class="logo-icon">
+              <img src={logo} />
+            </span>
+            <h1 class="logo-title">
+              <span>Integration</span>
+              <span>Lighthouse</span>
+            </h1>
           </div>
-        )}
+        </div>
+        <div class="app-header-spacer"></div>
+        <div class="app-header-actions">
+          <div class="app-header-spacer"></div>
+          <div>
+            <button class="user-profile">
+            <a class="user-profile" href="https://github.com/Integration-Lighthouse/Integration-Lighthouse/blob/main/README.md" target="_blank">Collaborate</a>
+            </button>
+          </div>
+        </div>
+      </header>
+      <div class="app-body">
+        <div class="app-body-navigation">
+          <nav class="navigation">
+            <div class="form-group field">
+              <input
+                type="input"
+                class="form-field"
+                placeholder="Search titles"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                name="search"
+                id="search"
+              />
+              <label for="search" class="form-label">
+                Search titles...
+              </label>
+            </div>
+            <div class="list">
+              <ul className="article-list">
+                {filteredArticles.map((firstLine, index) => (
+                  <li className="article-list-item">
+                    <button className="article-list-button"
+                      onClick={() =>
+                        handleArticleClick(firstLines.indexOf(firstLine))
+                      }
+                    >
+                      {firstLine || "Loading..."}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div class="app-body-main-content">
+          <div className="content-area">
+            {content ? (
+              <MarkDown>{content}</MarkDown>
+            ) : (
+              <div className="welcome-message">
+                <h2>Welcome to Integration Lighthouse!</h2>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
